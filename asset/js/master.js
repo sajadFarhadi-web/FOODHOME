4cef3bb2055199774464dbe6568c4ab08f1f5cdb
sajad farhadi <122044544+sajadFarhadi-web@users.noreply.github.com>
2023-05-31 11:47:14 +0330

    Add files via upload

---
+++ asset/js/master.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll' ,() =>{
+    const nav = document.querySelector('nav')
+        if(window.scrollY > 0 ){
+           nav.classList.add('scrolled')
+        } else {
+            nav.classList.remove('scrolled')
+        }
+    console.log(header);
+})
